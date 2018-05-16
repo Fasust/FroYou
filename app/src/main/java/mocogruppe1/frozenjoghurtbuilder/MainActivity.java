f613d314fd70b4ccca22ad2eca5689682ce90bc8
Dario Giuseppe Lazzara (inf518) <dario_giuseppe.lazzara@smail.th-koeln.de>
2018-05-16 10:17:11 +0200

    needed firebase datas added + first try to build up a realtime database

--- app/src/main/java/mocogruppe1/frozenjoghurtbuilder/MainActivity.java
+++ app/src/main/java/mocogruppe1/frozenjoghurtbuilder/MainActivity.java
@@ -2,12 +2,20 @@ package mocogruppe1.frozenjoghurtbuilder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase testbase = FirebaseDatabase.getInstance(); 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView testdata = (TextView)findViewById(R.id.testdata);
+
     }
 }
