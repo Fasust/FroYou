b8b843c08ae9e940818373fa25c86ecd44140839
Sebastian Faust <sebastianf333@googlemail.com>
2018-05-18 14:14:21 +0200

    Added Empty Activitys and connected them to Layout XMLs

---
+++ app/src/main/java/mocogruppe1/frozenjoghurtbuilder/OrderProcess.java
@@ -0,0 +1,14 @@
+package mocogruppe1.frozenjoghurtbuilder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class OrderProcess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orderprocess);
+
+    }
+}
---
+++ app/src/main/java/mocogruppe1/frozenjoghurtbuilder/RecipeDetail.java
@@ -0,0 +1,14 @@
+package mocogruppe1.frozenjoghurtbuilder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RecipeDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipedetail);
+
+    }
+}
