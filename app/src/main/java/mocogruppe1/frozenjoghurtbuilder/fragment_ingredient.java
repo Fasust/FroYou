abae2800d3a3a1962db062405468cdcdc6b3aa76
Julian Schoemaker <julian.schoemaker@smail.th-koeln.de>
2018-05-30 11:05:11 +0200

    changed java code of the list item fragment and removed the google+ button

--- app/src/main/java/mocogruppe1/frozenjoghurtbuilder/fragment_ingredient.java
+++ app/src/main/java/mocogruppe1/frozenjoghurtbuilder/fragment_ingredient.java
@@ -7,24 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.plus.PlusOneButton;
-
-/**
- * A fragment with a Google +1 button.
- */
 public class fragment_ingredient extends Fragment {
-
-    // The request code must be 0 or greater.
-    private static final int PLUS_ONE_REQUEST_CODE = 0;
-    // The URL to +1.  Must be a valid URL.
-    private final String PLUS_ONE_URL = "http://developer.android.com";
-    private PlusOneButton mPlusOneButton;
-
 
     public fragment_ingredient() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,19 +19,9 @@ public class fragment_ingredient extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_ingredient, container, false);
 
-        //Find the +1 button
-        mPlusOneButton = (PlusOneButton) view.findViewById(R.id.plus_one_button);
-
         return view;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        // Refresh the state of the +1 button each time the activity receives focus.
-        mPlusOneButton.initialize(PLUS_ONE_URL, PLUS_ONE_REQUEST_CODE);
-    }
 
 
 }
