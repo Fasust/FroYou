b74f769841c0aafd94ab6a98b012c0ceb7e917ee
Julian Schoemaker <julian.schoemaker@smail.th-koeln.de>
2018-06-11 21:22:45 +0200

    implemented interface of recipe gallery and detail

--- app/src/main/java/frozenyogurtbuilder/app/RecipeGallery.java
+++ app/src/main/java/frozenyogurtbuilder/app/RecipeGallery.java
@@ -10,27 +10,11 @@ import android.widget.ImageButton;
 
 public class RecipeGallery extends AppCompatActivity {
 
-    //Buttons
-    ImageButton btn_goTo_reDe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipegallery);
-
-        //Action Bar
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //getSupportActionBar().setTitle("Rezept Galerie");
-
-        btn_goTo_reDe = findViewById(R.id.btn_goTo_reDe) ;
-        btn_goTo_reDe.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RecipeGallery.this, RecipeDetail.class));
-            }
-
-        });
 
     }
 }
