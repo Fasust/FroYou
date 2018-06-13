41e81d7fbdba523103865a448cdbbe95900c6207
Dario Giuseppe Lazzara (inf518) <dario_giuseppe.lazzara@smail.th-koeln.de>
2018-06-13 18:26:12 +0200

    edit impressum things

---
+++ app/src/main/java/frozenyogurtbuilder/app/Impressum.java
@@ -0,0 +1,17 @@
+package frozenyogurtbuilder.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Impressum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_impressum);
+
+    }
+}
