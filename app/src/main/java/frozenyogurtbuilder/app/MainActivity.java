ed6de3d1143d72942f307e4c7f4bd911888d574f
Sebastian Faust <sebastianf333@googlemail.com>
2018-06-16 11:32:39 +0200

    Firestore added to gradle

--- app/src/main/java/frozenyogurtbuilder/app/MainActivity.java
+++ app/src/main/java/frozenyogurtbuilder/app/MainActivity.java
@@ -5,13 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
