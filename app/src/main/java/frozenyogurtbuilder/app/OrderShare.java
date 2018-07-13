e56e59d770952224e112e93c1e5d053acc78879c
Julian Schoemaker <julian.schoemaker@smail.th-koeln.de>
2018-07-13 14:12:33 +0200

    fixed camera - added permission in share order activity

--- app/src/main/java/frozenyogurtbuilder/app/OrderShare.java
+++ app/src/main/java/frozenyogurtbuilder/app/OrderShare.java
@@ -1,13 +1,18 @@
 package frozenyogurtbuilder.app;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -65,6 +70,16 @@ public class OrderShare extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_share_order);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED) {
+            Log.d("PERMISSION", "Camera granted");
+        } else {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 500);
+            }
+        }
 
         imageView_picture = findViewById(R.id.imageView_picture);
         nameEdit = findViewById(R.id.editText_name);
